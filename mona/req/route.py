5a0907b18d7f178882a971a903ff2495f5e09f77
Katun Ilya <7923715-katunilya@users.noreply.gitlab.com>
2022-03-09 16:06:46 +0000

    Resolve "Expand routing"

--- mona/req/route.py
+++ mona/req/route.py
@@ -1,10 +1,53 @@
+import re
+from typing import Match, Optional, Pattern
+
 from mona import context, state
 
 
-def on_route(path: str) -> context.Handler:
-    """If `path` is the same as request `path` than context is valid."""
+def on_route(pattern: str) -> context.Handler:
+    """If request `path` is the same as `pattern` than context is valid."""
+    pattern = pattern.strip("/")
 
     def _handler(ctx: context.Context) -> context.StateContext:
-        return state.valid(ctx) if path == ctx.path else state.invalid(ctx)
+        return state.valid(ctx) if pattern == ctx.path else state.invalid(ctx)
+
+    return _handler
+
+
+def on_subroute(pattern: str) -> context.Handler:
+    """Returns valid context without subroute part of it if `pattern` equals `path`."""
+    pattern = pattern.strip("/")
+
+    def _handler(ctx: context.Context) -> context.Context:
+        if ctx.path.startswith(pattern):
+            subroute = len(pattern)
+            ctx.path = ctx.path[subroute:].strip("/")
+            return state.valid(ctx)
+
+        return state.invalid(ctx)
+
+    return _handler
+
+
+def on_ciroute(pattern: str) -> context.Handler:
+    """Case insensitive version of `on_route`."""
+    pattern = pattern.strip("/").lower()
+
+    def _handler(ctx: context.Context) -> context.Context:
+        return state.valid(ctx) if ctx.path.lower() == pattern else state.invalid(ctx)
+
+    return _handler
+
+
+def on_cisubroute(pattern: str) -> context.Handler:
+    """Case insensitive version of `on_subroute`."""
+    pattern = pattern.strip("/").lower()
+
+    def _handler(ctx: context.Context) -> context.Context:
+        if ctx.path.lower().startswith(pattern):
+            subroute_len = len(pattern)
+            ctx.path = ctx.path[subroute_len:].strip("/")
+            return state.valid(ctx)
+        return state.invalid(ctx)
 
     return _handler
