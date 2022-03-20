f98f5005b71519b947cce0730a2f9be4489ca5bf
Katun Ilya <7923715-katunilya@users.noreply.gitlab.com>
2022-03-20 14:00:15 +0000

    Resolve "Expand body serialization/deserialization"

--- mona/req/events.py
+++ mona/req/events.py
@@ -1,6 +1,15 @@
+from typing import Type
+
+from pydantic import BaseModel
+
 from mona import context, handler
 
-from .body import parse_body_json
+from .body import (
+    parse_json_to_dataclass,
+    parse_json_to_dict,
+    parse_json_to_pydantic,
+    parse_to_string,
+)
 
 
 async def receive_body(ctx: context.Context) -> context.Context:
@@ -17,8 +26,43 @@ async def receive_body(ctx: context.Context) -> context.Context:
     return ctx
 
 
-# TODO make `receive_json` a function with docstring
-receive_json = handler.compose(
-    receive_body,
-    parse_body_json,
-)
+async def receive_json_dict(ctx: context.Context) -> context.Context:
+    """Read entire request body and parse it directly to dict from json string."""
+    # TODO [OPTIONAL] for optimization purpose this might be done directly, but it will
+    #                 be harder to support this code in future
+    _handler = handler.compose(
+        receive_body,
+        parse_json_to_dict,
+    )
+
+    return await _handler(ctx)
+
+
+def receive_json_dataclass(dataclass_: Type) -> context.Handler:
+    """Read entire request body and parse it directly to dataclass from json string."""
+
+    return handler.compose(
+        receive_body,
+        parse_json_to_dataclass(dataclass_),
+    )
+
+
+def receive_json_pydantic(pydantic_model: Type[BaseModel]) -> context.Handler:
+    """Read entire request body and parse it directly to `BaseModel` from json."""
+
+    return handler.compose(
+        receive_body,
+        parse_json_to_pydantic(pydantic_model),
+    )
+
+
+async def receive_string(ctx: context.Context) -> context.Context:
+    """Read entire request body and decode it to string with UTF-8."""
+    # TODO [OPTIONAL] for optimization purpose this might be done directly, but it will
+    #                 be harder to support this code in future
+    _handler = handler.compose(
+        receive_body,
+        parse_to_string,
+    )
+
+    return await _handler(ctx)
--- tests/test_request_route.py
+++ tests/test_request_route.py
@@ -1,4 +1,5 @@
 import pytest
+
 from mona import context, req, state
 
 
