0c12d1ba68cfd0cfc05b9b206af024ed4242d247
Katun Ilya <katun.ilya@gmail.com>
2022-03-26 16:02:53 +0300

    Add aprse_query with tests

--- mona/req/__init__.py
+++ mona/req/__init__.py
@@ -5,5 +5,6 @@ from .body import (
     parse_to_string,
 )
 from .events import receive_body, receive_json_dict
+from .query import parse_query
 from .route import on_ciroute, on_cisubroute, on_route, on_subroute
 from .type import on_http
