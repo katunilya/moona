684cb79812cad4fe8f5eeb5e9aac50a8358137b7
Ilya Katun <katun.ilya@gmail.com>
2022-04-14 22:41:59 +0300

    Fix flake8 error

--- examples/example_future.py
+++ examples/example_future.py
@@ -3,15 +3,15 @@ import asyncio
 from mona import future
 
 
-async def async_inc(x: int) -> int:
+async def async_inc(x: int) -> int:  # noqa
     return x + 1
 
 
-def sync_square(x: int) -> int:
+def sync_square(x: int) -> int:  # noqa
     return x**2
 
 
-async def main():
+async def main():  # noqa
     f = future.from_value(3)  # create some Future from sync value
 
     composition = future.compose(async_inc, async_inc, sync_square)  # (x + 1 + 1)^2
