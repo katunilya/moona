8a2e25c641437a8daaa2abc3f5c09f5b1c75a7af
Ilya Katun <katun.ilya@gmail.com>
2022-04-15 11:02:27 +0300

    Add res.set_body_from_text handler (#12)
    * Add res.set_body_from_str handler
    
    * Add tests for res.set_body_from_text

--- tests/test_response_body.py
+++ tests/test_response_body.py
@@ -1,5 +1,3 @@
-import typing
-
 import pydantic
 import pytest
 
@@ -90,78 +88,75 @@ def test_set_body_text(
     )
 
 
-# Functions for test-cases of `set_body_from_bytes`
+# Common functions for dynamic body set
+ERROR_MESSAGE = "Error Message"
+BYTES_ERROR_MESSAGE = ERROR_MESSAGE.encode("UTF-8")
 
 
-def sync_set_right_concrete_bytes(bts: typing.ByteString):  # noqa
-    return lambda ctx: state.right(bts)
+def sync_set_right_concrete_body(data):  # noqa
+    return lambda _: state.right(data)
 
 
-def sync_set_error_concrete_bytes(ctx):  # noqa
-    return state.error(error.Error("sync_set_error_concrete_bytes Error occurred."))
+def sync_set_error_body(_):
+    return state.error(error.Error(ERROR_MESSAGE))
 
 
-def async_set_right_concrete_bytes(bts: typing.ByteString):  # noqa
-    async def _setter(ctx):
-        return state.right(bts)
+def async_set_right_concrete_body(data):  # noqa
+    async def _handler(_):
+        return state.right(data)
 
-    return _setter
+    return _handler
 
 
-async def async_set_error_concrete_bytes(ctx):  # noqa
-    return state.error(error.Error("async_set_error_concrete_bytes Error occurred."))
+async def async_set_error_body(_):  # noqa
+    return state.error(error.Error(ERROR_MESSAGE))
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "arrange_state,act_function,assert_state,assert_body",
     [
-        (state.RIGHT, sync_set_right_concrete_bytes(b""), state.RIGHT, b""),
-        (state.RIGHT, sync_set_right_concrete_bytes(b"ABCD"), state.RIGHT, b"ABCD"),
+        (state.RIGHT, sync_set_right_concrete_body(b""), state.RIGHT, b""),
+        (state.RIGHT, sync_set_right_concrete_body(b"ABCD"), state.RIGHT, b"ABCD"),
         (
             state.RIGHT,
-            sync_set_right_concrete_bytes(b'{"name": "John Doe"}'),
-            state.RIGHT,
-            b'{"name": "John Doe"}',
-        ),
-        (state.WRONG, sync_set_right_concrete_bytes(b""), state.WRONG, None),
-        (state.WRONG, sync_set_right_concrete_bytes(b"ABCD"), state.WRONG, None),
-        (
-            state.WRONG,
-            sync_set_right_concrete_bytes(b'{"name": "John Doe"}'),
-            state.WRONG,
-            None,
-        ),
-        (state.RIGHT, async_set_right_concrete_bytes(b""), state.RIGHT, b""),
-        (state.RIGHT, async_set_right_concrete_bytes(b"ABCD"), state.RIGHT, b"ABCD"),
-        (
-            state.RIGHT,
-            async_set_right_concrete_bytes(b'{"name": "John Doe"}'),
+            sync_set_right_concrete_body(b'{"name": "John Doe"}'),
             state.RIGHT,
             b'{"name": "John Doe"}',
         ),
-        (state.WRONG, async_set_right_concrete_bytes(b""), state.WRONG, None),
-        (state.WRONG, async_set_right_concrete_bytes(b"ABCD"), state.WRONG, None),
+        (state.WRONG, sync_set_right_concrete_body(b""), state.WRONG, None),
+        (state.WRONG, sync_set_right_concrete_body(b"ABCD"), state.WRONG, None),
         (
             state.WRONG,
-            async_set_right_concrete_bytes(b'{"name": "John Doe"}'),
+            sync_set_right_concrete_body(b'{"name": "John Doe"}'),
+            state.WRONG,
+            None,
+        ),
+        (state.RIGHT, async_set_right_concrete_body(b""), state.RIGHT, b""),
+        (state.RIGHT, async_set_right_concrete_body(b"ABCD"), state.RIGHT, b"ABCD"),
+        (
+            state.RIGHT,
+            async_set_right_concrete_body(b'{"name": "John Doe"}'),
+            state.RIGHT,
+            b'{"name": "John Doe"}',
+        ),
+        (state.WRONG, async_set_right_concrete_body(b""), state.WRONG, None),
+        (state.WRONG, async_set_right_concrete_body(b"ABCD"), state.WRONG, None),
+        (
+            state.WRONG,
+            async_set_right_concrete_body(b'{"name": "John Doe"}'),
             state.WRONG,
             None,
         ),
         (
             state.RIGHT,
-            sync_set_error_concrete_bytes,
+            sync_set_error_body,
             state.ERROR,
-            b"sync_set_error_concrete_bytes Error occurred.",
+            BYTES_ERROR_MESSAGE,
         ),
-        (
-            state.RIGHT,
-            async_set_error_concrete_bytes,
-            state.ERROR,
-            b"async_set_error_concrete_bytes Error occurred.",
-        ),
-        (state.WRONG, sync_set_error_concrete_bytes, state.WRONG, None),
-        (state.WRONG, async_set_error_concrete_bytes, state.WRONG, None),
+        (state.RIGHT, async_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
+        (state.WRONG, sync_set_error_body, state.WRONG, None),
+        (state.WRONG, async_set_error_body, state.WRONG, None),
     ],
 )
 async def test_set_body_from_bytes(
@@ -183,106 +178,74 @@ async def test_set_body_from_bytes(
     assert ctx.value.response.body == assert_body
 
 
-# Functions for test-cases of `set_body_from_dict`
-
-
-def sync_set_right_concrete_dict(dct: dict):  # noqa
-    return lambda ctx: state.right(dct)
-
-
-def sync_set_error_concrete_dict(ctx):  # noqa
-    return state.error(error.Error("sync_set_error_concrete_dict Error occurred."))
-
-
-def async_set_right_concrete_dict(dct: dict):  # noqa
-    async def _setter(ctx):
-        return state.right(dct)
-
-    return _setter
-
-
-async def async_set_error_concrete_dict(ctx):  # noqa
-    return state.error(error.Error("async_set_error_concrete_dict Error occurred."))
-
-
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "arrange_state,act_function,assert_state,assert_body",
     [
-        (state.RIGHT, sync_set_right_concrete_dict({}), state.RIGHT, b"{}"),  # noqa
+        (state.RIGHT, sync_set_right_concrete_body({}), state.RIGHT, b"{}"),  # noqa
         (
             state.RIGHT,
-            sync_set_right_concrete_dict({"name": "John Doe"}),
-            state.RIGHT,
-            b'{"name":"John Doe"}',
-        ),
-        (
-            state.RIGHT,
-            sync_set_right_concrete_dict(
-                {"name": "John Doe", "info": {"age": 21, "city": "SPb"}}
-            ),
-            state.RIGHT,
-            b'{"name":"John Doe","info":{"age":21,"city":"SPb"}}',
-        ),
-        (
-            state.RIGHT,
-            sync_set_error_concrete_dict,
-            state.ERROR,
-            b"sync_set_error_concrete_dict Error occurred.",
-        ),
-        (state.RIGHT, async_set_right_concrete_dict({}), state.RIGHT, b"{}"),  # noqa
-        (
-            state.RIGHT,
-            async_set_right_concrete_dict({"name": "John Doe"}),
+            sync_set_right_concrete_body({"name": "John Doe"}),
             state.RIGHT,
             b'{"name":"John Doe"}',
         ),
         (
             state.RIGHT,
-            async_set_right_concrete_dict(
+            sync_set_right_concrete_body(
                 {"name": "John Doe", "info": {"age": 21, "city": "SPb"}}
             ),
             state.RIGHT,
             b'{"name":"John Doe","info":{"age":21,"city":"SPb"}}',
         ),
+        (state.RIGHT, sync_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
+        (state.RIGHT, async_set_right_concrete_body({}), state.RIGHT, b"{}"),  # noqa
         (
             state.RIGHT,
-            async_set_error_concrete_dict,
-            state.ERROR,
-            b"async_set_error_concrete_dict Error occurred.",
+            async_set_right_concrete_body({"name": "John Doe"}),
+            state.RIGHT,
+            b'{"name":"John Doe"}',
         ),
-        (state.WRONG, sync_set_right_concrete_dict({}), state.WRONG, None),  # noqa
+        (
+            state.RIGHT,
+            async_set_right_concrete_body(
+                {"name": "John Doe", "info": {"age": 21, "city": "SPb"}}
+            ),
+            state.RIGHT,
+            b'{"name":"John Doe","info":{"age":21,"city":"SPb"}}',
+        ),
+        (state.RIGHT, async_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
+        (state.WRONG, sync_set_right_concrete_body({}), state.WRONG, None),  # noqa
         (
             state.WRONG,
-            sync_set_right_concrete_dict({"name": "John Doe"}),
+            sync_set_right_concrete_body({"name": "John Doe"}),
             state.WRONG,
             None,
         ),
         (
             state.WRONG,
-            sync_set_right_concrete_dict(
+            sync_set_right_concrete_body(
                 {"name": "John Doe", "info": {"age": 21, "city": "SPb"}}
             ),
             state.WRONG,
             None,
         ),
-        (state.WRONG, sync_set_error_concrete_dict, state.WRONG, None),
-        (state.WRONG, async_set_right_concrete_dict({}), state.WRONG, None),  # noqa
+        (state.WRONG, sync_set_error_body, state.WRONG, None),
+        (state.WRONG, async_set_right_concrete_body({}), state.WRONG, None),  # noqa
         (
             state.WRONG,
-            async_set_right_concrete_dict({"name": "John Doe"}),
+            async_set_right_concrete_body({"name": "John Doe"}),
             state.WRONG,
             None,
         ),
         (
             state.WRONG,
-            async_set_right_concrete_dict(
+            async_set_right_concrete_body(
                 {"name": "John Doe", "info": {"age": 21, "city": "SPb"}}
             ),
             state.WRONG,
             None,
         ),
-        (state.WRONG, async_set_error_concrete_dict, state.WRONG, None),
+        (state.WRONG, async_set_error_body, state.WRONG, None),
     ],
 )
 async def test_set_body_from_dict(
@@ -320,128 +283,95 @@ class ExtendedUser(pydantic.BaseModel):  # noqa
     info: UserInfo
 
 
-def sync_set_right_concrete_pydantic_model(model: pydantic.BaseModel):  # noqa
-    return lambda ctx: state.right(model)
-
-
-def sync_set_error_concrete_pydantic_model(ctx):  # noqa
-    return state.error(
-        error.Error("sync_set_error_concrete_pydantic_model Error occurred.")
-    )
-
-
-def async_set_right_concrete_pydantic_model(model: pydantic.BaseModel):  # noqa
-    async def _setter(ctx):
-        return state.right(model)
-
-    return _setter
-
-
-async def async_set_error_concrete_pydantic_model(ctx):  # noqa
-    return state.error(
-        error.Error("async_set_error_concrete_pydantic_model Error occurred.")
-    )
-
-
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
     "arrange_state,act_function,assert_state,assert_body",
     [
         (
             state.RIGHT,
-            sync_set_right_concrete_pydantic_model(pydantic.BaseModel()),
+            sync_set_right_concrete_body(pydantic.BaseModel()),
             state.RIGHT,
             b"{}",  # noqa
         ),
         (
             state.RIGHT,
-            sync_set_right_concrete_pydantic_model(User(name="John Doe")),
+            sync_set_right_concrete_body(User(name="John Doe")),
             state.RIGHT,
             b'{"name":"John Doe"}',
         ),
         (
             state.RIGHT,
-            sync_set_right_concrete_pydantic_model(
+            sync_set_right_concrete_body(
                 ExtendedUser(name="John Doe", info=UserInfo(age=21, city="SPb"))
             ),
             state.RIGHT,
             b'{"name":"John Doe","info":{"age":21,"city":"SPb"}}',
         ),
+        (state.RIGHT, sync_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
         (
             state.RIGHT,
-            sync_set_error_concrete_pydantic_model,
-            state.ERROR,
-            b"sync_set_error_concrete_pydantic_model Error occurred.",
-        ),
-        (
-            state.RIGHT,
-            async_set_right_concrete_pydantic_model(pydantic.BaseModel()),
+            async_set_right_concrete_body(pydantic.BaseModel()),
             state.RIGHT,
             b"{}",  # noqa
         ),
         (
             state.RIGHT,
-            sync_set_right_concrete_pydantic_model(User(name="John Doe")),
+            sync_set_right_concrete_body(User(name="John Doe")),
             state.RIGHT,
             b'{"name":"John Doe"}',
         ),
         (
             state.RIGHT,
-            async_set_right_concrete_pydantic_model(
+            async_set_right_concrete_body(
                 ExtendedUser(name="John Doe", info=UserInfo(age=21, city="SPb"))
             ),
             state.RIGHT,
             b'{"name":"John Doe","info":{"age":21,"city":"SPb"}}',
         ),
-        (
-            state.RIGHT,
-            async_set_error_concrete_pydantic_model,
-            state.ERROR,
-            b"async_set_error_concrete_pydantic_model Error occurred.",
-        ),
+        (state.RIGHT, async_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
         #
         (
             state.WRONG,
-            sync_set_right_concrete_pydantic_model(pydantic.BaseModel()),
+            sync_set_right_concrete_body(pydantic.BaseModel()),
             state.WRONG,
             None,
         ),
         (
             state.WRONG,
-            sync_set_right_concrete_pydantic_model(User(name="John Doe")),
+            sync_set_right_concrete_body(User(name="John Doe")),
             state.WRONG,
             None,
         ),
         (
             state.WRONG,
-            sync_set_right_concrete_pydantic_model(
+            sync_set_right_concrete_body(
                 ExtendedUser(name="John Doe", info=UserInfo(age=21, city="SPb"))
             ),
             state.WRONG,
             None,
         ),
-        (state.WRONG, sync_set_error_concrete_pydantic_model, state.WRONG, None),
+        (state.WRONG, sync_set_error_body, state.WRONG, None),
         (
             state.WRONG,
-            async_set_right_concrete_pydantic_model(pydantic.BaseModel()),
+            async_set_right_concrete_body(pydantic.BaseModel()),
             state.WRONG,
             None,
         ),
         (
             state.WRONG,
-            sync_set_right_concrete_pydantic_model(User(name="John Doe")),
+            sync_set_right_concrete_body(User(name="John Doe")),
             state.WRONG,
             None,
         ),
         (
             state.WRONG,
-            async_set_right_concrete_pydantic_model(
+            async_set_right_concrete_body(
                 ExtendedUser(name="John Doe", info=UserInfo(age=21, city="SPb"))
             ),
             state.WRONG,
             None,
         ),
-        (state.WRONG, async_set_error_concrete_pydantic_model, state.WRONG, None),
+        (state.WRONG, async_set_error_body, state.WRONG, None),
     ],
 )
 async def test_set_body_from_pydantic(
@@ -454,6 +384,109 @@ async def test_set_body_from_pydantic(
     # arrange
     ctx = future.from_value(state.pack(arrange_state, mock_context))
     handler = res.set_body_from_pydantic(act_function)
+
+    # act
+    ctx: context.StateContext = await (ctx >> handler)
+
+    # assert
+    assert ctx.state == assert_state
+    assert ctx.value.response.body == assert_body
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "arrange_state,act_function,assert_state,assert_body",
+    [
+        (state.RIGHT, sync_set_right_concrete_body(""), state.RIGHT, b""),
+        (
+            state.RIGHT,
+            sync_set_right_concrete_body("Hello, World"),
+            state.RIGHT,
+            b"Hello, World",
+        ),
+        (
+            state.RIGHT,
+            sync_set_right_concrete_body('{"name": "John Doe"}'),
+            state.RIGHT,
+            b'{"name": "John Doe"}',
+        ),
+        (
+            state.RIGHT,
+            sync_set_right_concrete_body(
+                '{"name": "John Doe", "info": {"age": 21, "city": "SPb"}}'
+            ),
+            state.RIGHT,
+            b'{"name": "John Doe", "info": {"age": 21, "city": "SPb"}}',
+        ),
+        (state.RIGHT, sync_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
+        (state.RIGHT, async_set_right_concrete_body(""), state.RIGHT, b""),
+        (
+            state.RIGHT,
+            async_set_right_concrete_body("Hello, World"),
+            state.RIGHT,
+            b"Hello, World",
+        ),
+        (
+            state.RIGHT,
+            async_set_right_concrete_body('{"name": "John Doe"}'),
+            state.RIGHT,
+            b'{"name": "John Doe"}',
+        ),
+        (
+            state.RIGHT,
+            async_set_right_concrete_body(
+                '{"name": "John Doe", "info": {"age": 21, "city": "SPb"}}'
+            ),
+            state.RIGHT,
+            b'{"name": "John Doe", "info": {"age": 21, "city": "SPb"}}',
+        ),
+        (state.RIGHT, async_set_error_body, state.ERROR, BYTES_ERROR_MESSAGE),
+        (state.WRONG, sync_set_right_concrete_body(""), state.WRONG, None),
+        (state.WRONG, sync_set_right_concrete_body("Hello, World"), state.WRONG, None),
+        (
+            state.WRONG,
+            sync_set_right_concrete_body('{"name": "John Doe"}'),
+            state.WRONG,
+            None,
+        ),
+        (
+            state.WRONG,
+            sync_set_right_concrete_body(
+                '{"name": "John Doe", "info": {"age": 21, "city": "SPb"}}'
+            ),
+            state.WRONG,
+            None,
+        ),
+        (state.WRONG, sync_set_error_body, state.WRONG, None),
+        (state.WRONG, async_set_right_concrete_body(""), state.WRONG, None),
+        (state.WRONG, async_set_right_concrete_body("Hello, World"), state.WRONG, None),
+        (
+            state.WRONG,
+            async_set_right_concrete_body('{"name": "John Doe"}'),
+            state.WRONG,
+            None,
+        ),
+        (
+            state.WRONG,
+            async_set_right_concrete_body(
+                '{"name": "John Doe", "info": {"age": 21, "city": "SPb"}}'
+            ),
+            state.WRONG,
+            None,
+        ),
+        (state.WRONG, async_set_error_body, state.WRONG, None),
+    ],
+)
+async def test_set_body_from_text(
+    mock_context: context.Context,
+    arrange_state,
+    act_function,
+    assert_state,
+    assert_body,
+):
+    # arrange
+    ctx = future.from_value(state.pack(arrange_state, mock_context))
+    handler = res.set_body_from_text(act_function)
 
     # act
     ctx: context.StateContext = await (ctx >> handler)
