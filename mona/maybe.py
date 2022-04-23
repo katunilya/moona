497b736e040dfa5090aa3d455735eaefc483638b
Ilya Katun <katun.ilya@gmail.com>
2022-04-23 14:06:02 +0300

    Add Maybe base impl (#4)
    * `Maybe` container
    * `Some` container
    * `_Nothing` singletone for empty value
    * `Nothing` instance
    * `bind` function

---
+++ mona/maybe.py
@@ -0,0 +1,71 @@
+import abc
+import dataclasses
+import typing
+
+import toolz
+
+from mona import state
+
+T = typing.TypeVar("T")
+V = typing.TypeVar("V")
+
+
+@dataclasses.dataclass(frozen=True)
+class Maybe(state.State[T], abc.ABC):
+    """General purpose container for optional value.
+
+    `Maybe` container has 2 variations: `Some` and `Nothing`. `Some` stands for some
+    actual value and means that it is present in execution context. `Nothing` on the
+    other hand describes emtpiness and lack of value. This is additional wrapping around
+    `None` values.
+
+    Strict types help ignore
+    """
+
+
+@dataclasses.dataclass(frozen=True)
+class Some(Maybe[T]):
+    """Container for actually present value."""
+
+
+@dataclasses.dataclass(frozen=True)
+class _Nothing(Maybe[typing.Any]):
+    """Private container for non-existent value."""
+
+    __slots__ = ()
+    __instance: "_Nothing | None" = None
+
+    def __new__(cls, *args, **kwargs) -> "_Nothing":
+        match cls.__instance:
+            case None:
+                cls.__instance = object.__new__(cls)
+                return cls.__instance
+            case _:
+                return cls.__instance
+
+    def __init__(self) -> None:
+        super().__init__(None)
+
+
+Nothing = _Nothing()
+
+
+@toolz.curry
+def bind(function: typing.Callable[[Maybe[T]], Maybe[V]], cnt: Maybe[T]) -> Maybe[V]:
+    """Bind function for `Maybe` monad.
+
+    `function` is executed only in case `cnt` is `Some` and not `Nothing` as it does not
+    make any sense to execute some `function` with `Nothing`.
+
+    Args:
+        function (typing.Callable[[Maybe[T]], Maybe[V]]): to bind
+        cnt (Maybe[T]): `Maybe` container
+
+    Returns:
+        Maybe[V]: result of running `function` on `cnt` value.
+    """
+    match cnt:
+        case Some():
+            return function(cnt)
+        case _:
+            return cnt
