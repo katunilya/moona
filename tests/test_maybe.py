74778607a3594692886a0a3a7393806a1c925668
Ilya Katun <katun.ilya@gmail.com>
2022-04-23 14:13:22 +0300

    Add tests for `Maybe` monad (#4)
    * `test_nothing_is_singleton`
    * `test_bind` (including >> syntax)

---
+++ tests/test_maybe.py
@@ -0,0 +1,27 @@
+import pytest
+
+from mona import maybe
+
+
+def test_nothing_is_singleton():
+    assert maybe._Nothing() == maybe.Nothing
+
+
+@pytest.mark.parametrize(
+    "arrange_function,arrange_cnt,assert_cnt",
+    [
+        (lambda x: x, maybe.Some(1), maybe.Some(1)),
+        (lambda x: x, maybe.Some(2), maybe.Some(2)),
+        (lambda x: x, maybe.Nothing, maybe.Nothing),
+        (lambda x: maybe.Nothing, maybe.Nothing, maybe.Nothing),
+        (lambda x: maybe.Nothing, maybe.Some(1), maybe.Nothing),
+    ],
+)
+def test_bind(arrange_function, arrange_cnt, assert_cnt):
+    # act
+    act_cnt = maybe.bind(arrange_function, arrange_cnt)
+    act_cnt_rshift = arrange_cnt >> arrange_function
+
+    # assert
+    assert act_cnt == assert_cnt
+    assert act_cnt_rshift == assert_cnt
