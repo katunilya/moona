8ef0c633a881d65f4b49cbba7e0367ca506a062f
Ilya Katun <katun.ilya@gmail.com>
2022-04-30 20:51:48 +0300

    Rm old (#30)

--- mona/req/type.py
+++
@@ -1,8 +0,0 @@
-from mona import types
-from mona.monads import state
-
-
-@state.accepts_right
-def on_http(ctx: types.Context) -> types.StateContext:
-    """Returns right `Context` when request type is "http"."""
-    return state.Right(ctx) if ctx.request.type_ == "http" else state.Wrong(ctx)
