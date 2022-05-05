b5585d647574144593ba8f3c24c2fab0886542fb
Ilya Katun <katun.ilya@gmail.com>
2022-05-05 23:03:58 +0300

    Improve `mona.handlers.method` (#28)

--- mona/handlers/method.py
+++ mona/handlers/method.py
@@ -29,12 +29,66 @@ def method(method_: str) -> HTTPHandler:
     return _method
 
 
-GET = method("GET")
-POST = method("POST")
-PATCH = method("PATCH")
-PUT = method("PUT")
-DELETE = method("DELETE")
-OPTIONS = method("OPTIONS")
-HEAD = method("HEAD")
-TRACE = method("TRACE")
-CONNECT = method("CONNECT")
+__GET = method("GET")
+__POST = method("POST")
+__PATCH = method("PATCH")
+__PUT = method("PUT")
+__DELETE = method("DELETE")
+__OPTIONS = method("OPTIONS")
+__HEAD = method("HEAD")
+__TRACE = method("TRACE")
+__CONNECT = method("CONNECT")
+
+
+@http_handler
+def GET(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on GET method."""
+    return __GET(ctx)
+
+
+@http_handler
+def POST(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on POST method."""
+    return __POST(ctx)
+
+
+@http_handler
+def PATCH(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on PATCH method."""
+    return __PATCH(ctx)
+
+
+@http_handler
+def PUT(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on PUT method."""
+    return __PUT(ctx)
+
+
+@http_handler
+def DELETE(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on DELETE method."""
+    return __DELETE(ctx)
+
+
+@http_handler
+def OPTIONS(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on OPTIONS method."""
+    return __OPTIONS(ctx)
+
+
+@http_handler
+def HEAD(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on HEAD method."""
+    return __HEAD(ctx)
+
+
+@http_handler
+def TRACE(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on TRACE method."""
+    return __TRACE(ctx)
+
+
+@http_handler
+def CONNECT(ctx: HTTPContext) -> HTTPContextResult:  # noqa
+    """`HTTPContext` handler that proceeds `HTTPContext` only on CONNECT method."""
+    return __CONNECT(ctx)
