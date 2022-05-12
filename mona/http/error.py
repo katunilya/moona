db68c164ef9163eee11dabc722f98ecef3e86cd0
Ilya Katun <katun.ilya@gmail.com>
2022-05-12 23:18:52 +0300

    Add error handling for http (#42)

--- mona/http/error.py
+++ mona/http/error.py
@@ -1,9 +1,19 @@
 from mona.core import ErrorContext
-from mona.handlers.status import set_status
-from mona.http.core import HTTPContext
 from mona.monads.future import Future
 from mona.monads.pipe import Pipeline
 
+from .body import set_response_body_text
+from .core import HTTPContext, async_handler
+from .events import send_response_body_async
+from .status import set_status
 
+
+@async_handler
 def send_error_async(err: ErrorContext) -> Future[HTTPContext]:
-    return Pipeline(err.ctx).then(set_status(err.status))
+    """Sends error when one happens in the system."""
+    return (
+        Pipeline(err.ctx)
+        .then(set_status(err.status))
+        .then(set_response_body_text(err.message))
+        .then_future(send_response_body_async)
+    )
