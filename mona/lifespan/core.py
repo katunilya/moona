1fac508326648f3e1b022c08b4216f57e9eba56d
Ilya Katun <katun.ilya@gmail.com>
2022-05-12 23:09:52 +0300

    Add lifespan handling (#42)

--- mona/lifespan/core.py
+++ mona/lifespan/core.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Awaitable, Callable
+from functools import wraps
+from typing import Awaitable, Callable, TypeVar
 
-from mona.core import BaseContext, Message, Receive, Scope, Send
+from mona.core import BaseContext, ErrorContext, Message, Receive, Scope, Send
+from mona.monads.future import Future
 from mona.monads.pipe import Pipeline
 
 
@@ -44,72 +46,152 @@ class LifespanContext(BaseContext):
         self.__receive = receive
         self.__send = send
 
-    @staticmethod
-    def send(
-        message: Message,
-    ) -> Callable[[LifespanContext], Awaitable[LifespanContext]]:
-        """`LifespanContext` sends passed message.
 
-        Args:
-            message (Message): to send.
-        """
+T = TypeVar("T")
+V = TypeVar("V")
 
-        async def _send(ctx: LifespanContext) -> LifespanContext:
-            await ctx.__send(message)
-            return ctx
 
-        return _send
+def handler(func: Callable[[T], V]) -> Callable[[LifespanContext], V | T]:
+    """Decorator that protects sync function from non-`Lifespancontext` arg."""
 
-    def send_startup_complete_async(self) -> Awaitable[LifespanContext]:
-        """Sends "lifespan.startup.complete" event.
+    @wraps(func)
+    def _wrapper(ctx: T) -> V | T:
+        match ctx:
+            case LifespanContext():
+                return func(ctx)
+            case other:
+                return other
 
-        Note:
-            [Docs](https://asgi.readthedocs.io/en/latest/specs/lifespan.html#startup-complete-send-event)
-        """
-        return Pipeline(self).then_future(
-            LifespanContext.send({"type": "lifespan.startup.complete"})
-        )
+    return _wrapper
 
-    def send_shutdown_complete_async(self) -> Awaitable[LifespanContext]:
-        """Sends "lifespan.shutdown.complete" event.
 
-        Note:
-            [Docs](https://asgi.readthedocs.io/en/latest/specs/lifespan.html#shutdown-complete-send-event)
-        """
-        return Pipeline(self).then_future(
-            LifespanContext.send({"type": "lifespan.shutdown.complete"})
-        )
+def async_handler(
+    func: Callable[[T], Awaitable[V]]
+) -> Callable[[LifespanContext], Awaitable[V | T]]:
+    """Decorator that protects async function from non-`Lifespancontext` arg."""
 
-    @staticmethod
-    def send_startup_failed_async(
-        message: str = "",
-    ) -> Callable[[LifespanContext], Awaitable[LifespanContext]]:
-        """Send "lifespan.startup.failed" event.
+    @wraps(func)
+    async def _wrapper(ctx: T) -> V | T:
+        match ctx:
+            case LifespanContext():
+                return await func(ctx)
+            case other:
+                return other
 
-        Args:
-            message (str, optional): Info about reasons for failure. Defaults to "".
-        """
-        return LifespanContext.send(
-            {"type": "lifespan.startup.failed", "message": message}
-        )
+    return _wrapper
 
-    @staticmethod
-    def send_shutdown_failed_async(
-        message: str = "",
-    ) -> Callable[[LifespanContext], Awaitable[LifespanContext]]:
-        """Send "lifespan.shutdown.failed" event.
 
-        Args:
-            message (str, optional): Info about reasons for failure. Defaults to "".
-        """
-        return LifespanContext.send(
-            {"type": "lifespan.shutdown.failed", "message": message}
-        )
+def send_async(
+    message: Message,
+) -> Callable[[LifespanContext], Awaitable[LifespanContext]]:
+    """`LifespanContext` sends passed message.
 
-    def receive(self) -> Awaitable[Message]:
-        """Receive some Message during handling "lifespan" scope.
+    Args:
+        message (Message): to send.
+    """
 
-        Returns:
-            Awaitable[Message]: received message.
-        """
-        return self.__receive()
+    @async_handler
+    async def _send(ctx: LifespanContext) -> LifespanContext:
+        await ctx.__send(message)
+        return ctx
+
+    return _send
+
+
+@async_handler
+def send_startup_complete_async(ctx: LifespanContext) -> Awaitable[LifespanContext]:
+    """Sends "lifespan.startup.complete" event.
+
+    Note:
+        [Docs](https://asgi.readthedocs.io/en/latest/specs/lifespan.html#startup-complete-send-event)
+    """
+    return Pipeline(ctx).then_future(send_async({"type": "lifespan.startup.complete"}))
+
+
+@async_handler
+def send_shutdown_complete_async(ctx: LifespanContext) -> Awaitable[LifespanContext]:
+    """Sends "lifespan.shutdown.complete" event.
+
+    Note:
+        [Docs](https://asgi.readthedocs.io/en/latest/specs/lifespan.html#shutdown-complete-send-event)
+    """
+    return Pipeline(ctx).then_future(send_async({"type": "lifespan.shutdown.complete"}))
+
+
+def send_startup_failed_async(
+    message: str,
+) -> Callable[[LifespanContext], Awaitable[LifespanContext]]:
+    """Send "lifespan.startup.failed" event.
+
+    Args:
+        message (str, optional): Info about reasons for failure. Defaults to "".
+    """
+    return send_async({"type": "lifespan.startup.failed", "message": message})
+
+
+def send_shutdown_failed_async(
+    message: str,
+) -> Callable[[LifespanContext], Awaitable[LifespanContext]]:
+    """Send "lifespan.shutdown.failed" event.
+
+    Args:
+        message (str, optional): Info about reasons for failure. Defaults to "".
+    """
+    return send_async({"type": "lifespan.shutdown.failed", "message": message})
+
+
+@async_handler
+def receive_async(ctx: LifespanContext) -> Awaitable[Message]:
+    """Receive some Message during handling "lifespan" scope.
+
+    Returns:
+        Awaitable[Message]: received message.
+    """
+    return ctx.__receive()
+
+
+LifespanContextHandler = Callable[[LifespanContext], LifespanContext | ErrorContext]
+AsyncLifespanContextHandler = Callable[
+    [LifespanContext], Awaitable[LifespanContext | ErrorContext]
+]
+
+
+def lifespan_async(
+    on_startup: AsyncLifespanContextHandler,
+    on_shutdown: AsyncLifespanContextHandler,
+) -> LifespanContextHandler:
+    """Handler for "lifespan" scope type.
+
+    `on_startup` is called on server startup receiving "lifespan.startup" event and
+    `on_shutdown` is called on server shutdown receiving "lifespan.shutdown" event.
+
+    Args:
+        on_startup (LifespanHandler): to call on server startup.
+        on_shutdown (LifespanHandler): to call on server shutdown.
+    """
+
+    @handler
+    async def _lifespan_async(ctx: LifespanContext) -> LifespanContext:
+        # This infinite loop ensures that lifespan scope persists over entire
+        # application life cycle. Specific for "lifespan" scope.
+        while True:
+            match await ctx.__receive():
+                case {"type": "lifespan.startup"}:
+                    match await Future(on_startup(ctx)):
+                        case LifespanContext():
+                            await send_startup_complete_async(ctx)
+                        case ErrorContext() as err:
+                            await Pipeline(err.ctx).then_future(
+                                send_startup_failed_async(err.message)
+                            )
+                case {"type": "lifespan.shutdown"}:
+                    match await Future(on_shutdown(ctx)):
+                        case LifespanContext():
+                            await send_shutdown_complete_async(ctx)
+                        case ErrorContext() as err:
+                            await Pipeline(err.ctx).then_future(
+                                send_shutdown_failed_async(err.message)
+                            )
+                    return ctx
+
+    return _lifespan_async
