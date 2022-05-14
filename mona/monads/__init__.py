5202a4a3c062100d6300ab6ee0ca78cd20ff5c17
Ilya Katun <katun.ilya@gmail.com>
2022-05-14 18:53:26 +0300

    Remove monads and old handlers for http (#42)

--- mona/monads/__init__.py
+++
@@ -1,5 +0,0 @@
-from .func import Func, FutureFunc
-from .future import Future
-from .maybe import FutureMaybe, Maybe, Nothing, Some
-from .pipe import Pipeline
-from .result import Error, FutureResult, Ok, Result
