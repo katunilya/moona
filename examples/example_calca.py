1724515f69e8d511fcaefa15a27d206303a688b9
Ilya Katun <katun.ilya@gmail.com>
2022-05-16 22:56:09 +0300

    Improve calca example (#42)

--- examples/example_calca.py
+++ examples/example_calca.py
@@ -1,61 +1,25 @@
 from math import factorial, sqrt
 
-import moona
-from moona.context import HTTPContext
-from moona.handlers import (
-    GET,
-    bind_body_text_async,
-    choose,
-    compose,
-    get_body_text_async,
-    http,
-    route,
-    send_body_async,
-)
-from moona.handlers.core import HTTPContextResult
-from moona.monads.func import FutureFunc
-from moona.monads.future import Future
-from moona.monads.pipe import Pipeline
-from moona.monads.result import Result
+from moona import asgi, http
 
 
 def fibonacci(n: int) -> int:  # noqa
     return int((((1 + sqrt(5)) ** n) - ((1 - sqrt(5))) ** n) / (2**n * sqrt(5)))
 
 
-h = FutureFunc(get_body_text_async).then(int).then(fibonacci).then(str)
+def handle_fibonacci(n: int) -> http.HTTPHandler:  # noqa
+    return http.text(str(fibonacci(n)))
 
 
-def fibonacci_handler(ctx: HTTPContext) -> Future[HTTPContextResult]:  # noqa
-    return (
-        Pipeline(ctx)
-        .then(GET)
-        .then(route("/fibonacci"))
-        .then_future(bind_body_text_async())
-        .then_future(send_body_async)
-    )
+def handle_factorial(n: int) -> http.HTTPHandler:  # noqa
+    return http.text(str(factorial(n)))
 
 
-factorial_handler = compose(
-    GET,
-    route("/factorial"),
-    bind_body_text_async(
-        compose(
-            get_body_text_async,
-            Result.if_ok_safe(int),
-            Result.if_ok_safe(factorial),
-            Result.if_ok_safe(str),
-        )
-    ),
-    send_body_async,
+http_handler = http.GET >> http.choose(
+    [
+        http.route("/factorial") >> http.bind_int(handle_factorial),
+        http.route("/fibonacci") >> http.bind_int(handle_fibonacci),
+    ]
 )
 
-app = moona.create(
-    compose(
-        http,
-        choose(
-            fibonacci_handler,
-            factorial_handler,
-        ),
-    )
-)
+app = asgi.create(http_handler=http_handler)
