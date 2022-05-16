8f898e21907d42e6d907a3d7187f96f653eabe4b
Ilya Katun <katun.ilya@gmail.com>
2022-05-16 22:38:15 +0300

    Improve asgi create function (#42)

--- moona/asgi.py
+++ moona/asgi.py
@@ -1,7 +1,47 @@
 from moona import http, lifespan
 from moona.context import ASGIApp, Receive, Scope, Send
 
-_default_http_handler = http.send_text("Empty server")
+
+async def _handle_lifespan(
+    scope: Scope,
+    receive: Receive,
+    send: Send,
+    *,
+    startup_handler: lifespan.LifespanHandler = None,
+    shutdown_handler: lifespan.LifespanHandler = None,
+) -> None:
+    ctx = lifespan.LifespanContext(scope, receive, send)
+    while True:
+        match await ctx.receive():
+            case {"type": "lifespan.startup"}:
+                match startup_handler:
+                    case None:
+                        await ctx.send({"type": "lifespan.startup.complete"})
+                    case _:
+                        await startup_handler(lifespan.end, ctx)
+                        await ctx.send({"type": "lifespan.startup.complete"})
+            case {"type": "lifespan.shutdown"}:
+                match shutdown_handler:
+                    case None:
+                        await ctx.send({"type": "lifespan.shutdown.complete"})
+                    case _:
+                        await shutdown_handler(lifespan.end, ctx)
+                        await ctx.send({"type": "lifespan.shutdown.complete"})
+                return
+
+
+_default_http_handler = http.not_implemented("Service is not implemented. Sorry!")
+
+
+async def _handle_http(
+    scope: Scope,
+    receive: Receive,
+    send: Send,
+    *,
+    http_handler: http.HTTPHandler = None,
+):
+    ctx = http.HTTPContext(scope, receive, send)
+    await http_handler(http.end, ctx)
 
 
 def create(
@@ -20,8 +60,11 @@ def create(
         * https://asgi.readthedocs.io/en/latest/specs/main.html#applications
 
     Args:
-        http (AsyncHTTPContextHandler): handler for "http" scope
-        lifespan (AsyncLifespanContextHandler): handler for "lifespan" scope
+        http_handler (HTTPHandler): optional argument for handling "http" requests.
+        on_startup_handler (LifespanHandler): optional for handlers to be executed on
+        server startup
+        on_shutdown_handler (LifespanHandler): optional for handlers to be executed on
+        server shutdown
 
     Returns:
         ASGIApp: ASGI function based on ASGI Specification.
@@ -30,34 +73,19 @@ def create(
     async def _asgi(scope: Scope, receive: Receive, send: Send) -> None:
         match scope:
             case {"type": "lifespan"}:
-                ctx = lifespan.LifespanContext(scope, receive, send)
-                while True:
-                    match await ctx.receive():
-                        case {"type": "lifespan.startup"}:
-                            match startup_handler:
-                                case None:
-                                    await ctx.send(
-                                        {"type": "lifespan.startup.complete"}
-                                    )
-                                case _:
-                                    await startup_handler(lifespan.end, ctx)
-                                    await ctx.send(
-                                        {"type": "lifespan.startup.complete"}
-                                    )
-                        case {"type": "lifespan.shutdown"}:
-                            match shutdown_handler:
-                                case None:
-                                    await ctx.send(
-                                        {"type": "lifespan.shutdown.complete"}
-                                    )
-                                case _:
-                                    await shutdown_handler(lifespan.end, ctx)
-                                    await ctx.send(
-                                        {"type": "lifespan.shutdown.complete"}
-                                    )
-                            return
+                await _handle_lifespan(
+                    scope,
+                    receive,
+                    send,
+                    startup_handler=startup_handler,
+                    shutdown_handler=shutdown_handler,
+                )
             case {"type": "http"}:
-                ctx = http.HTTPContext(scope, receive, send)
-                await http_handler(http.end, ctx)
+                await _handle_http(
+                    scope,
+                    receive,
+                    send,
+                    http_handler=http_handler,
+                )
 
     return _asgi
