eaf84ba2e6c7241038b447024293763970433264
Ilya Katun <katun.ilya@gmail.com>
2022-05-17 22:37:33 +0300

    Add tests for `content_type` handler (#42)

--- tests/http/test_response_headers.py
+++ tests/http/test_response_headers.py
@@ -1,6 +1,7 @@
 import pytest
 
-from moona.http import HTTPContext, end, header
+from moona.http import HTTPContext, end
+from moona.http.response_headers import content_type, header
 
 
 @pytest.mark.asyncio
@@ -15,3 +16,17 @@ from moona.http import HTTPContext, end, header
 async def test_header(ctx: HTTPContext, s_name, s_value, b_name, b_value):
     _ctx = await header(s_name, s_value)(end, ctx)
     assert _ctx.response_headers[b_name] == b_value
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "s_value, b_value",
+    [
+        ("application/json", b"application/json"),
+        ("text/plain", b"text/plain"),
+        ("form/multipart", b"form/multipart"),
+    ],
+)
+async def test_content_type(ctx: HTTPContext, s_value, b_value):
+    _ctx = await content_type(s_value)(end, ctx)
+    assert _ctx.response_headers[b"content-type"] == b_value
